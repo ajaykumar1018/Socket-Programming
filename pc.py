042dfcbc13d33f00e6e4000f8449cb8237a4c3d8
Ajay Kumar <42745121+ajaykumar1018@users.noreply.github.com>
2019-12-29 15:12:17 +0530

    Update and rename pcser.py to pc.py

--- pc.py
+++ pc.py
@@ -1,12 +1,12 @@
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('192.168.225.129', 80))
+serv.bind(('192.168.225.129', 80)) #your IP address and port
 serv.listen(5)
 print('start listening')
 while True:
     conn,addr = serv.accept()
     print(conn,addr)
     print('acceptdone')
-    conn.sendall(b'1')
+    conn.sendall(b'1')  # if you want to send strings then edit this line
     conn.close()
     print ('client disconnected')
